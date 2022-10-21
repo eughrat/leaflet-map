926b265857a03c69a8e95965aa1ac45aa95a2e57
eughrat <michal.piernicki@gmail.com>
2022-10-21 15:31:37 +0200

    add some style to leaflet map

--- src/Components/MyMap.jsx
+++ src/Components/MyMap.jsx
@@ -1,14 +1,27 @@
 import React, { Component } from "react";
 import { MapContainer, GeoJSON } from "react-leaflet";
-import countries from "./../Data/countries.json";
+import mapData from "./../Data/countries.json";
 import "leaflet/dist/leaflet.css";
+import "./MyMap.css";
 
 class MyMap extends Component {
     state = {}
 
     componentDidMount(){
-        console.log(countries);
+        const countryName = country.properties.ADMIN;
+        console.log(countryName);
     }
+
+    countryStyle = {
+        fillColor : "red",
+        fillOpacity : 0.1,
+        color : "black",
+        weight : 2,
+    };
+
+    onEachCountry = (country, layer) => {
+        console.log(country.properties.ADMIN);
+    };
 
     render() {
         return (
@@ -16,7 +29,8 @@ class MyMap extends Component {
             <h1 style = {{textAlign: "center"}}>
                 My Map
             </h1>
-            <MapContainer style = {{height: "80vh"}} zoom = {2} center = {[20,100]}>
+            <MapContainer style = {{height: "80vh"}} zoom = {5} center = {[20,100]}>
+                <GeoJSON style = { this.countryStyle } data = {mapData.features} onEachFeature = {this.OnEachCountry}></GeoJSON>
             </MapContainer>
         </div>
         );
